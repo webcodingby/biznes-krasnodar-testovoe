8887fe487ffd75052ea73e2ec309185596a29589
webcodingby <webcoindg.by@gmail.com>
2023-01-26 23:55:47 +0300

    add config and class App

--- app/core/App.php
+++ app/core/App.php
@@ -8,6 +8,20 @@ class App
 {
     public static function start()
     {
+        self::libs();
+    }
+
+    public static function libs()
+    {
+        $config = require_once 'config/app.php';
+        foreach ($config['libs'] as $lib)
+        {
+            require_once "libs/" . $lib . ".php";
+        }
+    }
+
+    public static function db()
+    {
 
     }
 }
